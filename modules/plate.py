a9a003fe72fcb92c76cfe24592cf579862c0a22c
Nick Ladd <laddng@gmail.com>
2016-12-02 11:33:11 -0500

    basic contour searching for rectangles of certain area. low success rate

--- modules/plate.py
+++ modules/plate.py
@@ -9,33 +9,33 @@ class Plate:
 		self.plate_image = None;
 		self.gray_image = None;
 		self.plate_number = "";
+		self.roi = [];
 
 	def grayImage(self):
 		self.gray_image = cv2.cvtColor(self.original_image, cv2.COLOR_BGR2GRAY);
-		# apply a heavy gaussian blur
-		self.gray_image = cv2.GaussianBlur(self.gray_image, (45,45), 0);
+
+		# blur image to remove noise for contours
+		self.gray_image = cv2.GaussianBlur(self.gray_image, (29,29), 0);
 
 	def findContour(self):
 		self.grayImage();
 		ret, threshold = cv2.threshold(self.gray_image, 127, 255, 0);
-		image, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
-		potential_plates = [];
-		w = 0;
+		_,contours,_ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE);
 
-		print("[findContour]: "+str(len(contours))+" contours found...");
+		w,h,x,y = 0,0,0,0;
 
 		for contour in contours:
 			area = cv2.contourArea(contour);
-			if area > 200 and area < 400:
-				[w,h,x,y] = cv2.boundingRect(contour);
+			if area > 6000 and area < 40000:
+				[x,y,w,h] = cv2.boundingRect(contour);
 
-			if w > 50:
-				potential_plates.append(contour);
-				cv2.fillPoly(self.original_image, pts=contour, color=(255,255,255));
+			if w > 100 and w < 200 and h > 60 and h < 100:
+				self.roi.append(contour);
+				cv2.rectangle(self.original_image, (x,y), (x+w, y+h), (0,255,0), 15);
 
-		#final_image = cv2.drawContours(self.original_image, potential_plates, -1, (0,255,0), 4);
+		print("[findContour]: "+str(len(self.roi))+" potential plates found...");
 
 		plt.figure();
-		plt.imshow(final_image);
+		plt.imshow(self.original_image);
 		plt.show();
 
