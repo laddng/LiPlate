46890e55e38b4abfd636fcad5398ad2e87f885a0
Nick Ladd <laddng@gmail.com>
2016-12-02 15:08:59 -0500

    basic license plate found

--- modules/plate.py
+++ modules/plate.py
@@ -8,7 +8,7 @@ class Plate:
 		self.original_image = image;
 		self.plate_image = None;
 		self.gray_image = None;
-		self.plate_number = "";
+		self.plate_number = "None";
 		self.roi = [];
 
 	def grayImage(self):
@@ -40,7 +40,7 @@ class Plate:
 			# rough dimensions of a license plate
 			if w > 100 and w < 200 and h > 60 and h < 100:
 				self.roi.append([x,y,w,h]);
-				cv2.rectangle(self.original_image, (x,y), (x+w, y+h), (0,255,0), 15);
+				cv2.rectangle(self.original_image, (x,y), (x+w, y+h), (0,255,0), 10);
 
 		print("[findContour]: "+str(len(self.roi))+" potential plates found...");
 		return True;
@@ -53,11 +53,28 @@ class Plate:
 
 	def showResults(self):
 		plt.figure();
-		plt.subplot(211);
+
+		# original image
+		plt.subplot(321);
 		plt.imshow(self.original_image);
+		plt.xlabel("Original image");
+
+		# threshold image
+		plt.subplot(322);
+		plt.imshow(self.gray_image);
+		plt.xlabel("Threshold image");
+
+		# plate located
+		plt.subplot(323);
+		plt.imshow(self.original_image);
+		plt.xlabel("Plate located");
+
+		# plate cropped
 		if self.plate_image is not None:
-			plt.subplot(212);
+			plt.subplot(324);
 			plt.imshow(self.plate_image);
+			plt.xlabel("License plate #: "+str(self.plate_number));
+
 		plt.show();
 		return True;
 
